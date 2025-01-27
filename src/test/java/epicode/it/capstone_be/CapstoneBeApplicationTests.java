0566a5cc5e8b4322c1f0357109d03421c7767965
Ikadev95 <fegughi@gmail.com>
2025-01-27 09:45:59 +0100

    Initial commit

---
+++ src/test/java/epicode/it/capstone_be/CapstoneBeApplicationTests.java
@@ -0,0 +1,13 @@
+package epicode.it.capstone_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
